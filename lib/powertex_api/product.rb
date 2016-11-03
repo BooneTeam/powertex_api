3c367427d0f996b89e450bc43551305591b9ccc4
Garrett Boone <Boone.Garrett@gmail.com>
2016-11-03 17:10:35 -0500

    add sku fdl base

--- lib/powertex_api/product.rb
+++ lib/powertex_api/product.rb
@@ -5,8 +5,17 @@ module PowertexApi
 
       def all_by_base_sku
         PowertexApi::Api.get_request('/api/products') do |response|
-          powertex_products  = response['$values']
+          powertex_products = response['$values']
           powertex_products.group_by { |product| product['baseSku'] }
+        end
+      end
+
+      def items_grouped_by_product_sku_fdl_base
+        PowertexApi::Api.get_request('/api/products') do |response|
+          powertex_products = response['$values']
+          powertex_products.group_by do |product|
+            /(\w*-\w*)-?/.match(product['sku']).captures[0]
+          end
         end
       end
 
@@ -16,7 +25,7 @@ module PowertexApi
       # So pull out the stuff after | as a variation
       def all_by_name_split_by_pipe
         PowertexApi::Api.get_request('/api/products') do |response|
-          powertex_products  = response['$values']
+          powertex_products = response['$values']
           powertex_products.group_by { |p| p['name'].split('|').first }
         end
       end
